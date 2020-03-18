f6b577468a5dfbeb50c6d1d6d24647dcad075b4d
awave1 <artgolovin96@gmail.com>
2020-03-18 15:15:10 -0600

    refactor: remove unused imports

--- src/DeBruijnConversion.hs
+++ src/DeBruijnConversion.hs
@@ -3,10 +3,6 @@ module DeBruijnConversion
   )
 where
 
-import           Data.Map                       ( Map )
-import qualified Data.Map                      as Map
-import           Data.Set                       ( Set )
-import qualified Data.Set                      as Set
 import           Data.List
 import           Data.Maybe
 
